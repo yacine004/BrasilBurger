7ce0d7bf1b624e71f496322e5df801e79b3ec78e
yacine004 <yacine004@users.noreply.github.com>
2025-12-29 02:25:24 +0000

    fix: Retirer monolog-bundle et sa config pour éviter les conflits Composer

--- symfony/config/bundles.php
+++ symfony/config/bundles.php
@@ -6,7 +6,6 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    // Symfony\Bundle\MonologBundle\MonologBundle sera installé par Composer lors du build Docker
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
